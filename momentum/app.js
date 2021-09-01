64e492d81bc3cf192e66b945442c23982bd97624
designerasun <asuntheawesome@gmail.com>
2021-09-02 00:22:15 +0900

    chrome momentum clone coding exercise

---
+++ momentum/app.js
@@ -0,0 +1,13 @@
+alert("hi!");
+
+// Create a constant variable
+const a = 5; 
+const b = 2;
+const veryLongVariableName = 0; // CamelCase in Javascript c.f. snake_case in Python
+const myName = "jake";
+
+console.log(a+b); 
+console.log(a*b); 
+console.log(a-b); 
+
+console.log("hello " + myName);
