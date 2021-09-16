58eb0689756c5282e8b546156809e2017714e548
designerasun <asuntheawesome@gmail.com>
2021-09-17 00:03:08 +0900

    JSON.stringify, JSON.parse, and arrow func in JS

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,13 +2,16 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoFormInput = document.querySelector("#todo-form input"); 
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = []; 
+let toDos = []; 
+const TODOS_KEY = "to-dos"; 
+const savedToDos = localStorage.getItem(TODOS_KEY); 
 
 function saveToDos(){ 
     // localStorage does not support array. Below toDos(array)'s element 
     // will be added individually in a text format unless JSON.stringify used
     // Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-    localStorage.setItem("to-dos", JSON.stringify(toDos));
+    // JSON.stringify(array to string) <-> JSON.parse(string to array)
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 
 }
 
@@ -45,7 +48,6 @@ function paintToDo(newToDo){
 
     // Delete when button clicked
     toDoBtn.addEventListener("click", deleteToDo);
-    
 }
 
 function deleteToDo(event){
@@ -58,3 +60,26 @@ function deleteToDo(event){
 
 toDoForm.addEventListener("submit", handleToDoSubmit); 
 
+function sayHello(item){
+    // parsedToDos.forEach(sayHello);  
+    // sayHello(the first element of parsedToDos); 
+    // sayHello(the second element of parsedToDos);  
+    // ...
+    // sayHello(the last element of parsedToDos);
+    console.log(`hi! ${item}`); 
+}
+
+if (savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos); 
+    // console.log(parsedToDos.length); 
+
+    toDos = parsedToDos; 
+
+    // forEach function in JS: Performs the specified action for each element in an array.
+    // arrow function in Javascript : a shorter way of writing a function in JS
+    // (argument) => (return); OR (params) => (expression); 
+    // e.g1 - unnamed function : (item) => console.log(`hi! ${item}`);
+    // e.g2 - named function : let myFirstArrowFunc = (a,b) => console.log(a+b); ------> myFirstArrowFunc(3,4);  
+    // arrow function is easy to read but has its own limitation
+    parsedToDos.forEach(paintToDo); 
+}
