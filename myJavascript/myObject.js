fa82f5e7a3076a61064fd87fc7b433966ba55b4d
designerasun <asuntheawesome@gmail.com>
2021-10-21 23:34:34 +0900

    object creation in JS

---
+++ myJavascript/myObject.js
@@ -0,0 +1,11 @@
+// Create an object in Javascript : using curly braces
+const personObjectFirst = {"name":"jake", "age":27, "sex":"male"}; 
+
+// c.f. create an array in Javascript : using square brackets
+const countryArray = ["United Kingdom", "United States", "Brazil"]; 
+
+// Create an object with new and Object keyword
+const personObjectSecond = new Object();
+personObjectSecond["name"] = "jake";
+personObjectSecond["age"] = 27;
+personObjectSecond["sex"] = "male"; 
