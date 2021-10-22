2d67d2d6cd0971e0b971bdb7df56cc6d424000af
designerasun <asuntheawesome@gmail.com>
2021-10-22 22:52:47 +0900

    for in statement in JS

--- myJavascript/myObject.js
+++ myJavascript/myObject.js
@@ -9,3 +9,30 @@ const personObjectSecond = new Object();
 personObjectSecond["name"] = "jake";
 personObjectSecond["age"] = 27;
 personObjectSecond["sex"] = "male"; 
+
+// console.log(typeof(personObjectSecond["sex"]));
+
+const myArr = ["first", "second", "third"]; 
+const myObj = {11:"first", 22:"second", 33:"third"}; 
+
+if (typeof(myArr.indexOf("first")===0)){
+    console.log("tru")
+} else {
+    console.log("not tru")
+}
+
+// for ~ in statement in Javascript
+// printing the indice of the arry
+for (myArrIndex in myArr) { 
+    console.log(myArrIndex, typeof(myArrIndex));
+}
+
+// printing the indice of the object
+for (myObjIndex in myObj) { 
+    console.log(myObjIndex, typeof(myObjIndex));
+}
+
+// printing the object's value
+for (myObjIndexSecond in myObj) { 
+    console.log(myObj[myObjIndexSecond]); 
+}
