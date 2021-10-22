e6ab2362ad6d7b355361abf8453d3f778f3c13d6
designerasun <asuntheawesome@gmail.com>
2021-10-22 22:53:33 +0900

    HTTP msg in JS explained

--- myJavascript/firstWebServer.js
+++ myJavascript/firstWebServer.js
@@ -1,6 +1,14 @@
 // Javascript's advance to server side
+// To use the HTTP server and client one must require('http').
 const http = require('http'); 
 
+// HTTP message headers are represented by an object like this:
+// { 'content-length': '123',
+// 'content-type': 'text/plain',
+// 'connection': 'keep-alive',
+// 'host': 'mysite.com',
+// 'accept': '*' }
+// Keys are lowercased. Values are not modified.
 http.createServer(function(req, res) { 
     res.writeHead(200, {'content-type':'text/plain'});
     res.write('Hello World');
