43b592ec110c2daf3348280a23c17b57c399bc50
designerasun <asuntheawesome@gmail.com>
2021-10-28 19:08:44 +0900

    module in javascript

---
+++ myJavascript/module/module_hello.js
@@ -0,0 +1,8 @@
+// In Javascript, module concept is slightly different in host environment.
+// e.g browser, server, app script ...
+// Exploring various language is required to have a understanding of
+// how JS works differently depending on each environment. 
+
+function helloWorld() {
+    alert("hello world"); 
+}
