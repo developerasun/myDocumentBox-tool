5ee3ad54d1a73807d840c29d6f96f1dfd5f84099
designerasun <asuntheawesome@gmail.com>
2021-11-03 22:02:21 +0900

    literal used for easier scripting

---
+++ myJavascript/this/literal.js
@@ -0,0 +1,25 @@
+
+// check if 'this' belongs to the created object or globalThis. 
+let thisStatus = null; 
+
+// create a function with Function constructor.
+const myFunc1 = new Function('x', 'y', 'return x+y;');
+console.log(myFunc1(1,2));
+
+// create a function with function literal (easier to script)
+function myFunc2(x,y) {
+    thisStatus = this;
+    if (thisStatus === globalThis) {
+        console.log("this not belongs to the object");
+    } else {
+        console.log("this belongs to the created object"); 
+    }
+    return x+y;
+}
+
+// this status comparision depending on constructor
+console.log("not created with constructor : ", myFunc2(1,2));
+
+const newMyFunc2 = new myFunc2();
+console.log("created with constructor : ", newMyFunc2); 
+
