b3b653cd2bc5ba01c2da6d4be64be5a84a94820a
designerasun <asuntheawesome@gmail.com>
2021-11-07 21:26:22 +0900

    replace, regExp, toUppercase, toLowerCase

---
+++ myJavascript/basicAlgorithm/firstCapital.js
@@ -0,0 +1,27 @@
+function titleCase(str) {
+  return str;
+// not solved yet
+  }
+  
+  titleCase("I'm a little tea pot");
+
+
+// logic 
+  // let test = "text".split("");
+  // test[0] = test[0].toUpperCase();
+  // test = test.join("")
+  // console.log(test); 
+
+
+  // let bowl = new Array();
+  // str = str.toLowerCase().split(" ");
+  // console.log(str);
+  
+  // let result = str.map((val)=> {
+  //   val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  // })
+
+  // result = result.join(" ");
+  
+  // console.log(result);
+  // return str;
