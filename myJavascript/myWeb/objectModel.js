50693036118d9eb958f016a47211eb311602972c
designerasun <asuntheawesome@gmail.com>
2021-11-08 20:41:31 +0900

    DOM and BOM in javascript

---
+++ myJavascript/myWeb/objectModel.js
@@ -0,0 +1,15 @@
+// The web browser elements are objects. And Javascript controls these 
+// objects to manipulate the web browser. 
+// DOM : Document Object Model - types of DOM objects : 
+// 1) window 2) document 3) history(url visits) 4) location 5) navigator
+// types of DOM tres nodes
+// 1) document 2) element 3) attribute 4) text 5) comment
+// BOM : Browser Object Model 
+
+// HTML img tag <=> browser image object
+// controling img tag with javascript <=> manipulating browser img object with javascript
+
+// (document object)      (javascript code)
+const myImage = document.querySelector(".myImage");
+// (docuemnt object property)   (javascript code)
+myImage.style.width = "300px"; 
