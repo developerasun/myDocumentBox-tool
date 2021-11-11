3edb25e06fe16d3606075ae9435dd9078b12e622
designerasun <asuntheawesome@gmail.com>
2021-11-11 19:53:35 +0900

    document,element.getElementby | attribute APIs | attribute vs property

--- myJavascript/myWeb/element/element.js
+++ myJavascript/myWeb/element/element.js
@@ -30,5 +30,40 @@ whatIsIt.classList.toggle("toggleToggle"); // delete toggle toggle class
 console.log(whatIsIt.classList);
 console.log(whatIsIt.classList.toggle("toggleToggle"));
 
+// document.getElementBy vs element.getElementBy
+const doc_get = document.getElementsByClassName("grabMe"); // document.getElementBy
+const elem_get = document.getElementById("grabMeUl");
+elem_get.getElementsByClassName("grabMe"); // element.getElementBy
 
+console.group("document vs element");
+console.log("doc_get :", doc_get);
+console.log("elem : ", elem_get);
+console.groupEnd(); 
 
+// Element attribute API
+// 1) Element.getAttribute(name)
+// 2) Element.setAttribute(name, value)
+// 3) Element.hasAttribute(name)
+// 4) Element.removeAttribute(name)
+const target = document.getElementById("target"); 
+console.group("Element attribute");
+console.log(target.getAttribute("href")); // result : www.google.com
+target.setAttribute("href", "https://www.youtube.com");
+console.log(target.getAttribute("href")); // result : www.youtube.com
+target.setAttribute("nickname", "hello target");
+console.log(target.getAttribute("nickname")); 
+target.removeAttribute("nickname");
+console.log(target.getAttribute("nickname")); // result : null
+console.log(target.hasAttribute("nickname")); // result : false
+console.groupEnd();
+
+// Comparison : attribute vs property
+// speed => attribute < property
+// sometimes both approach have the same result but sometimes not. 
+// For example, 
+console.group("attribute vs property");
+const attr_path = document.getElementById("path");
+console.log(attr_path.getAttribute("href")); // result : relative path
+const prop_path = document.getElementById("path");
+console.log(prop_path.href); // absolute path
+console.groupEnd(); 
