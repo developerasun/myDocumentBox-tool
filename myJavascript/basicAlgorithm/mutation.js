1ba6e62e00bd0634856d1de6c72a055212b62702
designerasun <asuntheawesome@gmail.com>
2021-11-13 22:47:16 +0900

    free code camp js algorithm mutation exercise : not solved yet

---
+++ myJavascript/basicAlgorithm/mutation.js
@@ -0,0 +1,34 @@
+function mutation(arr) {
+
+    // not solved yet
+
+  }
+  
+  mutation(["hello", "hely"]);
+
+
+  // trial 
+//   let first = arr[0]; 
+//   let second = arr[1];
+//   // let status = false; 
+//   let count = 0;
+
+//   first = first.split("").sort();
+//   second = second.split("").sort(); 
+//   console.log(first); 
+//   console.log(second); 
+
+//   for (let key in second) {
+//       for (let i=0; i<first.length; i++) {
+//           if (second[key] === first[i]) {
+//               count++;
+//           }
+//       } 
+//   }
+
+//   if (count >= second.length) { 
+//       console.log(count);
+//       console.log(true);
+//   } 
+
+//   return false;
