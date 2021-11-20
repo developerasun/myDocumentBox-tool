c71bcc717dc132395d358f63db3cf57cd7b86f2b
designerasun <asuntheawesome@gmail.com>
2021-11-21 00:06:34 +0900

    free code camp exercise error check: slice and splice

---
+++ myJavascript/functionalProgramming/test.js
@@ -0,0 +1,6 @@
+const socialWindow =['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']; // Social sites
+
+const testing = socialWindow.slice(0,2);
+
+// socialWindow.splice(0, 2);
+console.log(testing);
