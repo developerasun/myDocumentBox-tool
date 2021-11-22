0c004d025be85fb76d5ca8862e72492b0683c2dc
designerasun <asuntheawesome@gmail.com>
2021-11-22 19:37:24 +0900

    simple JSX exercise

--- myJavascript/myReact/jsx/my-app/src/index.js
+++ myJavascript/myReact/jsx/my-app/src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const whoWhere = function(countries){
+  return name + " from " + countries.usa;
+}
+
+const countries = {
+  usa : "Washington D.C",
+  canada : "Ottawa",
+};
+
+const name = "jake"; 
+const element = <h1>Hello {whoWhere(countries)}</h1>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
+
+
+// npm start : dev build, npm run build : production build(for minified JS file)
+// see result in localhost:3000
+// refer below commands to start/end react app
+// "scripts": {
+//   "start": "react-scripts start",
+//   "build": "react-scripts build",
+//   "test": "react-scripts test",
+//   "eject": "react-scripts eject"
+// },
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
