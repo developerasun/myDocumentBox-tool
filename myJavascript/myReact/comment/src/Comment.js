8d469a8f22699898d11ee56bfc1b4fdfd676549c
designerasun <asuntheawesome@gmail.com>
2021-11-24 09:30:10 +0900

    state exampled added, componenetDidMount method applied

--- myJavascript/myReact/comment/src/Comment.js
+++ myJavascript/myReact/comment/src/Comment.js
@@ -5,14 +5,15 @@ import avatar from "./avatar.png";
 class Comment extends React.Component {
     // render method should have a return statement
     render() {
+        const { name, content } = this.props; 
         return (
         <div>
             <div className="comment">
                 <div className="entity">
                     <img id="avatar" src={avatar} alt="default avatar"/> 
                     <div className="text-group">
-                        <h1 class="username">{this.props.name}</h1>
-                        <p className="content">{this.props.content}</p>
+                        <h1 class="username">{name}</h1>
+                        <p className="content">{content}</p>
                     </div>
                 </div>
             </div>
