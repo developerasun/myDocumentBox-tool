a55b176a1a4cb0ef8d7eee36dd60f70e709e7334
designerasun <asuntheawesome@gmail.com>
2021-11-29 21:08:00 +0900

    simple form component added

--- myJavascript/myReact/attendance/src/index.js
+++ myJavascript/myReact/attendance/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Attendance from './attendance';
 
 ReactDOM.render(
-  <Attendance />,
+  <App />,
   document.getElementById('root')
 );
 
