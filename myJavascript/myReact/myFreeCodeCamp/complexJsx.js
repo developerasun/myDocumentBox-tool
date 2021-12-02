a2b94d8991af8432efff6a13cd9245b8b1e733b2
designerasun <asuntheawesome@gmail.com>
2021-12-02 18:16:21 +0900

    FCC react basics

---
+++ myJavascript/myReact/myFreeCodeCamp/complexJsx.js
@@ -0,0 +1,14 @@
+// nested JSX must return a single element
+// meaning parenet element wrapping all the elements up
+
+const JSX = (
+    <div>
+        <h1></h1>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+);
---
+++ myJavascript/myReact/myFreeCodeCamp/statelessFx.js
@@ -0,0 +1,13 @@
+// creating a component : 1) function 2) class
+// functional component : 1) stateless 2) must return JSX or null
+
+const MyComponent = function() {
+    // Change code below this line
+    return (
+        // nested JSX must return a single element
+        <div>
+            <p>lorem ipsum</p>
+        </div> 
+    )
+    // Change code above this line
+  }
