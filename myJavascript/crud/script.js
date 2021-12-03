b2ac37bbc810a4062ccf1f56c7099d59a21fa3a7
designerasun <asuntheawesome@gmail.com>
2021-12-03 23:25:05 +0900

    HTML table and C out of CRUD in js

--- myJavascript/crud/script.js
+++ myJavascript/crud/script.js
@@ -1,48 +1,19 @@
 const submitBtn = document.querySelector("#submitBtn");
 const displayHere = document.querySelector("#display");
 const resetBtn = document.querySelector("#resetBtn");
+const formOutputTbody = document.querySelector("#formOutputTbody");
 
 submitBtn.addEventListener("click", (event)=>{
     event.preventDefault();
-    onSubmit();
+    createFormDataOutput(readFormData());
 });
 
 resetBtn.addEventListener("click", (event)=>{
     event.preventDefault();
-    resetFormData();
+    resetFormData((readFormData()));
 });
 
-function onSubmit() {
-    const getFormData = readFormData(); 
-    console.log(getFormData);
-    alert("Your form successfully submitted");
-    displayHere.innerHTML = 
-    ` 
-    <li>${getFormData.fullName}</li>
-    <li>${getFormData.empCode}</li>
-    <li>${getFormData.salary}</li>
-    <li>${getFormData.city}</li>
-    `;
-}
-
-function resetFormData() {
-    const getFormData = readFormData();
-
-    for (let key in getFormData) {
-        getFormData[key] = "";
-    }
-    
-    displayHere.innerHTML = 
-    ` 
-    <li>${getFormData.fullName}</li>
-    <li>${getFormData.empCode}</li>
-    <li>${getFormData.salary}</li>
-    <li>${getFormData.city}</li>
-    `;
-    console.log(getFormData);
-
-}
-
+// readFormData reads form inputs 
 function readFormData() {
     const formData = {}; // formData contains form input values
     formData.fullName = document.querySelector("#fullName").value;
@@ -51,3 +22,35 @@ function readFormData() {
     formData.city = document.querySelector("#city").value;
     return formData;
 }
+
+// createFormDataOutput creates form output 
+function createFormDataOutput(readFormData) {
+
+    // add a new row in the table
+    const newRow = formOutputTbody.insertRow(-1);
+
+    // add a new cell in the row
+    const nameCell = newRow.insertCell(0);
+    const empCell = newRow.insertCell(1);
+    const salaryCell = newRow.insertCell(2);
+    const cityCell = newRow.insertCell(3);
+    const editAndDeleteCell = newRow.insertCell(4);
+
+    // display form input data into the cells 
+    nameCell.innerHTML = readFormData.fullName;
+    empCell.innerHTML = readFormData.empCode; 
+    salaryCell.innerHTML = readFormData.salary; 
+    cityCell.innerHTML = readFormData.city;
+    editAndDeleteCell.innerHTML = `<a>Edit</a>
+                                   <a>Delete</a>`; 
+}
+
+// resetFormData resets form inputs 
+function resetFormData() {
+    const readFormData = createFormDataOutput();
+    readFormData.name = "";
+    readFormData.emp = "";
+    readFormData.salary = "";
+    readFormData.city = "";
+}
+
