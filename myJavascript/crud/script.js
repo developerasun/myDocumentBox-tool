2d79fcf17e0dd340ec4c9fea6b807fcb3bbb803a
designerasun <asuntheawesome@gmail.com>
2021-12-04 23:45:46 +0900

    reset, delete function added

--- myJavascript/crud/script.js
+++ myJavascript/crud/script.js
@@ -6,11 +6,12 @@ const formOutputTbody = document.querySelector("#formOutputTbody");
 submitBtn.addEventListener("click", (event)=>{
     event.preventDefault();
     createFormDataOutput(readFormData());
+    resetFormData();
 });
 
 resetBtn.addEventListener("click", (event)=>{
     event.preventDefault();
-    resetFormData((readFormData()));
+    resetFormData();
 });
 
 // readFormData reads form inputs 
@@ -33,7 +34,7 @@ function createFormDataOutput(readFormData) {
     const nameCell = newRow.insertCell(0);
     const empCell = newRow.insertCell(1);
     const salaryCell = newRow.insertCell(2);
-    const cityCell = newRow.insertCell(3);
+    const cityCell = newRow.insertCell(3)
     const editAndDeleteCell = newRow.insertCell(4);
 
     // display form input data into the cells 
@@ -41,16 +42,34 @@ function createFormDataOutput(readFormData) {
     empCell.innerHTML = readFormData.empCode; 
     salaryCell.innerHTML = readFormData.salary; 
     cityCell.innerHTML = readFormData.city;
-    editAndDeleteCell.innerHTML = `<a>Edit</a>
-                                   <a>Delete</a>`; 
+    editAndDeleteCell.innerHTML = `<a onClick="editFormData(this)">Edit</a>
+                                   <a onClick="deleteFormData(event)">Delete</a>`;
 }
 
 // resetFormData resets form inputs 
 function resetFormData() {
-    const readFormData = createFormDataOutput();
-    readFormData.name = "";
-    readFormData.emp = "";
-    readFormData.salary = "";
-    readFormData.city = "";
+    document.querySelector("#fullName").value = "";
+    document.querySelector("#empCode").value = "";
+    document.querySelector("#salary").value = "";
+    document.querySelector("#city").value = "";
 }
 
+// editFormData updates form inputs
+function editFormData(data) {
+
+}
+
+
+
+// deleteFormData deletes form inputs
+function deleteFormData(event) {
+
+    // confirm function returns a boolean value
+    if (confirm("Delete this data?")) { 
+        const row = event.target.parentNode.parentNode;
+        console.log(row);
+        console.log(row.rowIndex);
+        console.log("hello");
+        formOutputTbody.deleteRow(row.rowIndex-1);
+    }
+}
