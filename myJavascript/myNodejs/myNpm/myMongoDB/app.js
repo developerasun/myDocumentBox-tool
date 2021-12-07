ec4d2da73d0e431d8c77b0ec9a236a721a5dcf0b
designerasun <asuntheawesome@gmail.com>
2021-12-07 16:17:01 +0900

    auth and logout endpoint router added

--- myJavascript/myNodejs/myNpm/myMongoDB/app.js
+++ myJavascript/myNodejs/myNpm/myMongoDB/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const config = require('./config/key')
+const { auth } = require('./middleware/auth')
 
 // importing mongdo DB connection
  mongoose.connect(config.MONGO_URI, {
@@ -72,6 +73,29 @@ app.post('/login', function(req, res){
             
         } )
     })
+})
+
+// auth router : path, middleware, callback
+app.get('/auth', auth, (req, res)=>{
+
+    res.status(200).json({
+        _id : req.user._id,
+        isAdmin : req.user.role === 0 ? false : true, 
+        isAuth : true, 
+        email : req.user.email, 
+        name : req.user.name, 
+        lastname : req.user.lastname, 
+        role : req.user.role, 
+        image : req.user.image, 
+
+    })
+})
+
+app.get('/logout', auth, (req, res)=>{
+    User.findOneAndUpdate( {_id:req.user._id}, { token : ""}, (err, user)=>{
+        if(err) return res.json({success:false, err}); 
+        return res.status(200).send({success:true});
+    } )
 })
 
 app.listen(port, ()=>{console.log(`app listening on port ${port}`)})
