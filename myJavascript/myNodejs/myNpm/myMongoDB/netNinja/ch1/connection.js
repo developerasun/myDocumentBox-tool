ff8ff771181ea7689021fc38ceb8f331fe5e07c7
designerasun <asuntheawesome@gmail.com>
2021-12-08 17:46:26 +0900

    mocha hook : before, beforeEach added, dropping collections

--- myJavascript/myNodejs/myNpm/myMongoDB/netNinja/ch1/connection.js
+++ myJavascript/myNodejs/myNpm/myMongoDB/netNinja/ch1/connection.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose'); 
-const Mario = require('./model/mario');
 
-// connect to mongoDB
-mongoose.connect("mongodb://localhost/test")
-        .then(()=>{console.log("connected")})
-        .catch((err)=> {console.log(err)});;
+// ES6 Promises
+mongoose.Promise = global.Promise; 
 
-// import Mario model
-const testing = new Mario({name: "testing mario", weight:55});
-console.log(testing); 
+// before, beforeEach : Only available when invoked via the mocha CLI.
+// connect to mongoDB before test runs
+before(function(done){
+        mongoose.connect("mongodb://localhost/test")
+                .then(()=>{
+                        console.log("connected");
+                        done();
+                })
+                .catch((err)=> {console.log(err)});;
+});
+
+// Drop(delete) the character collections before each test
+beforeEach(function(){
+        mongoose.connection.collections.Marios.drop(function(done){
+                done();
+        })
+})
+
+// Save records with Mongoose methods : 1) find 2) findOne
+// 1) find : returns a multiple records matche
+// 2) findOne : find the first one matched and returns a single record
