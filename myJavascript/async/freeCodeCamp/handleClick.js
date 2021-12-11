f6add53ffafde77ceab997ba92edb68c0ccccb06
designerasun <asuntheawesome@gmail.com>
2021-12-11 19:39:31 +0900

    event : DOMContentLoaded with document

---
+++ myJavascript/async/freeCodeCamp/handleClick.js
@@ -0,0 +1,13 @@
+
+// DOMContentLoaded
+// event fires when the initial HTML document has been completed
+// loaded and parsed, without waiting for stylesheets, images, 
+// and subframes to finish loading.
+
+document.addEventListener('DOMContentLoaded', function(){
+// Add your code below this line
+
+document.getElementById('getMessage').onclick= function(){}
+// Add your code above this line
+});
+
