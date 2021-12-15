101d65163fb292080addab6368f28eab7aeb180c
designerasun <asuntheawesome@gmail.com>
2021-12-15 14:04:32 +0900

    built my first React hook

--- myJavascript/myReact/clock/src/index.js
+++ myJavascript/myReact/clock/src/index.js
@@ -4,23 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myClock = function() {
-  const localTime = new Date().toLocaleTimeString();
-  const element = (
-    <div>
-      <h1>Jake's clock</h1>
-      <p>Now it's : {localTime}</p>
-    </div> 
-  ); 
+// const myClock = function() {
+//   const localTime = new Date().toLocaleTimeString();
+//   const element = (
+//     <div>
+//       <h1>Jake's clock</h1>
+//       <p>Now it's : {localTime}</p>
+//     </div> 
+//   ); 
 
   ReactDOM.render(
-    element, 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
     document.getElementById('root')
   ); 
 
-}
 
-setInterval(myClock, 1000);
+
+// setInterval(myClock, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
