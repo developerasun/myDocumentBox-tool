0e2da0bc7570564e94230cbe505c4d8b0feac57e
designerasun <asuntheawesome@gmail.com>
2021-12-19 18:33:04 +0900

    finishing off learning es6 basics in Free code camp

---
+++ myJavascript/es6/importAll.js
@@ -0,0 +1,7 @@
+// * will import every export from your module
+import * as stringFunctions from './string_functions.js'
+
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
