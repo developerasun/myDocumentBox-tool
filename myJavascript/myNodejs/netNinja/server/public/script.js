46fc9cf0c8d52b72b6c89e14bbe9b3072e6e6ab1
designerasun <asuntheawesome@gmail.com>
2021-12-21 17:21:05 +0900

    public assets browser can use added

---
+++ myJavascript/myNodejs/netNinja/server/public/script.js
@@ -0,0 +1,8 @@
+console.log("hello express")
+const banner = document.querySelector("section")
+
+window.addEventListener("load", ()=>{
+    setTimeout(()=>{
+        banner.style.display = "block"
+    },2000)
+})
