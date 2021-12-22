d9809d3f41b4f4c81c755fb2ed820a7cd682027f
designerasun <asuntheawesome@gmail.com>
2021-12-22 16:10:52 +0900

    form validation, list render modules added

--- myJavascript/myTypescript/src/script.ts
+++ myJavascript/myTypescript/src/script.ts
@@ -1,33 +1,52 @@
 import Invoice from "./modules/invoice.js"
+import Payment from './modules/payment.js'
+import ListTemplate from './modules/listTemplate.js'
+import { HasFormatter } from "./interfaces/hasFormatter.js"
+import { Validate } from "./modules/validation.js"
 
-const invOne = new Invoice("Jake", "drink", 8)
-const invTwo = new Invoice("Elly", "house", 100)
-
-// variable : variable type = initial value
-let invoices:Invoice[]= []
-
-invoices.push(invOne, invTwo)
-
-invoices.forEach((item)=>{
-    console.log(item.client, item.amount, item.details, item.format())
-})
-
-// type casting in typescript
-const newForm = document.querySelector('.newForm') as HTMLFormElement
-console.log(newForm.children)
-
-// inputs 
 const type = document.querySelector("#type") as HTMLSelectElement
 const toFrom = document.querySelector("#toFrom") as HTMLInputElement
 const details = document.querySelector("#details") as HTMLInputElement
 const amounts = document.querySelector("#amounts") as HTMLInputElement
+const result = document.querySelector(".result") as HTMLUListElement
 
+// type casting
+type record = HasFormatter
+let recordOne : record
+let recordTwo : record
+let records : record[] = [ ]
 
+// recordOne can be either Invoice or Payment since both 
+// invoice and payment class implements HasFormatter interface
+recordOne = new Invoice("Jake", "netflix", 55)
+recordTwo = new Payment("Paul", "netflix-streaming", 55)
+
+records.push(recordOne, recordTwo)
+records.map((item)=>console.log(item.format()))
+
+// type casting in typescript
+const newForm = document.querySelector('.newForm') as HTMLFormElement
+
+// submit event
 newForm.addEventListener("submit", (e:Event)=> {
     e.preventDefault()
 
-    console.log(type.value)
-    console.log(toFrom.value)
-    console.log(details.value)
-    console.log(amounts.valueAsNumber)
+    if (Validate(amounts)) {
+        switch (type.value) {
+            case "Invoice" : 
+                const newInv = new Invoice(toFrom.value, details.value, parseInt(amounts.value))
+                const ul_invoice = new ListTemplate(result)
+                ul_invoice.render(newInv, type)
+                break
+            case "Payment" : 
+                const newPay = new Payment(toFrom.value, details.value, parseInt(amounts.value))
+                const ul_payment = new ListTemplate(result)
+                ul_payment.render(newPay, type)
+                break
+            default : 
+                alert("Please select option")
+        }
+    } else { 
+        alert("Amounts should be in number")
+    }
 })
