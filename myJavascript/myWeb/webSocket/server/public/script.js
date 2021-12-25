88094acafe8fea4ed116bd738182b6236428af43
designerasun <asuntheawesome@gmail.com>
2021-12-25 17:47:36 +0900

    learning websocket basics for chat app

---
+++ myJavascript/myWeb/webSocket/server/public/script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header")
+const dropDownItem = document.querySelector(".dropdown-item")
+
+header.addEventListener("mouseenter", ()=>{
+    dropDownItem.classList.remove("hidden")
+    dropDownItem.classList.add("dropdown")
+})
+
+header.addEventListener("mouseleave", ()=>{
+    dropDownItem.classList.remove("dropdown")
+    dropDownItem.classList.add("hidden")
+})
+
+const socket = io.connect('http://localhost:3000/')
