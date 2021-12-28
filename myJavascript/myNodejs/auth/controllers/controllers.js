860272025cc6ce2a100ab2f8589453c870f5b904
designerasun <asuntheawesome@gmail.com>
2021-12-28 17:58:41 +0900

    user login error handler added, async/await and try catch : prevent unhandled promise in node js

--- myJavascript/myNodejs/auth/controllers/controllers.js
+++ myJavascript/myNodejs/auth/controllers/controllers.js
@@ -1,5 +1,25 @@
 const User = require('../model/user')
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code)
+
+    let errors = { email : '', password : '' }
+
+    // User creates a duplicated ID(email)
+    if (err.code === 11000) {  
+        errors.email = "Already existing email!"
+        return errors
+    }
+
+    // MongoDB validator fails
+    if(err.message.includes('User validation failed')){
+        console.log(Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message
+        }))
+    }
+    return errors
+}
+
 const signup_get = (req, res) => {
     res.render('signup')
 }
@@ -14,13 +34,12 @@ const login_get = (req, res) => {
 
 const login_post = async (req, res) => {
     console.log(req.body)
-    // const { email, password } = req.body
     try {
         const user = await User.create(req.body)
         res.status(201).json(user)
     } catch(err) {
-        console.log(err)
-        res.status(400).send("error occured")
+        const errors = handleErrors(err)
+        res.status(400).json( { errors } )
     }
 }
 
