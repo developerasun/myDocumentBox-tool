0c01500dd3024208ce4ac0dbe6cba1dbfed65bca
designerasun <asuntheawesome@gmail.com>
2021-12-28 17:59:27 +0900

    understanding mongoose pre/post hooks, hashing user password with the pre hook

--- myJavascript/myNodejs/auth/model/user.js
+++ myJavascript/myNodejs/auth/model/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
     email : {
@@ -17,6 +18,34 @@ const userSchema = new Schema({
         unique : true, 
         minlength: [6, 'Password length should be long than 6']
     }
+})
+
+// schema.pre : Defines a pre hook for the model.
+// schema.pre fires before model is saved
+userSchema.pre('save', function(next){
+    console.log("user will be created", this)
+    next()
+})
+
+userSchema.pre('save', async function(next){
+    try {
+        const salt = await bcrypt.genSalt()
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    }
+    catch(err){
+        console.log(err)
+    }
+
+})
+
+// schema.post : Defines a post hook for the model.
+// Create a mongoose hook for password hashing
+// Works like a middleware, meaning if the next method is not fired, 
+// response will stop
+userSchema.post('save', function(doc, next){
+    console.log("new user created and saved", doc)
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
