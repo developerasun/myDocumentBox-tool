46e2ebef583f80e670effc86be5306c083dfea06
designerasun <asuntheawesome@gmail.com>
2021-12-29 15:47:11 +0900

    Render components added

--- myJavascript/myReact/net-ninja/src/App.js
+++ myJavascript/myReact/net-ninja/src/App.js
@@ -1,28 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import Navbar from "./components/query/navbar";
-import People from "./components/query/people";
-import Planets from "./components/query/planets";
 import nameContainer from "./components/query/stateNames/nameContainer";
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
-
+import RenderPlanet from "./components/query/planets";
+import RenderPeople from "./components/query/people";
 
 function App() {
   const [page, setPage] = useState(nameContainer.setPageConst.planet)
 
   return (
-    <div className="App">
-      <Navbar setPage={setPage}/>
-      <main>
-        <h1>Hello React Again</h1>
-        <QueryClientProvider client={queryClient}>
-        { page===nameContainer.setPageConst.planet 
-              ? <Planets /> : <People /> }
-        </QueryClientProvider>
-      </main>
-    </div>
+      <div className="App">
+        <Navbar setPage={setPage}/>
+        <main>
+          <h1 style={{"textAlign":"center"}}>React Query Exercise</h1>
+          { page===nameContainer.setPageConst.planet 
+                ? <RenderPlanet/> : <RenderPeople /> }
+        </main>
+      </div>
   );
 }
 
