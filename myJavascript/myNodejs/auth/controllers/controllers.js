7c043b1d81b231e9d499e22d8587aa5d6d210f52
designerasun <asuntheawesome@gmail.com>
2021-12-29 18:10:48 +0900

    handler endpoint fixed

--- myJavascript/myNodejs/auth/controllers/controllers.js
+++ myJavascript/myNodejs/auth/controllers/controllers.js
@@ -24,23 +24,24 @@ const signup_get = (req, res) => {
     res.render('signup')
 }
 
-const signup_post = (req, res) => {
-    res.send('signup post request')
+const signup_post = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.create( { email, password } )
+        res.status(201).json(user)
+    } catch(err) {
+        const errors = handleErrors(err)
+        res.status(400).json( { errors } )
+    }
 }
 
 const login_get = (req, res) => {
     res.render('login')
 }
 
-const login_post = async (req, res) => {
-    console.log(req.body)
-    try {
-        const user = await User.create(req.body)
-        res.status(201).json(user)
-    } catch(err) {
-        const errors = handleErrors(err)
-        res.status(400).json( { errors } )
-    }
+const login_post = (req, res) => {
+    const { email , password } = req.body
+    res.send("this is login post handler")
 }
 
 module.exports = { 
