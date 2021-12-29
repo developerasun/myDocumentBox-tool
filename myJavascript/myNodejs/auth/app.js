4b88bf6f9ed94ac72dec80ca8d1926d7817fa64e
designerasun <asuntheawesome@gmail.com>
2021-12-29 18:12:43 +0900

    middleware organized, cookie routers added

--- myJavascript/myNodejs/auth/app.js
+++ myJavascript/myNodejs/auth/app.js
@@ -2,21 +2,19 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/routes');
+const cookieParser = require('cookie-parser')
 
 const MONGO_URI = process.env.MONGO_URI
 const app = express();
 
-// middleware
-app.use(express.static('public'));
-
 // view engine
 app.set('view engine', 'ejs');
 
-// Express JSON parser middleware(JSON => js object)
-app.use(express.json())
-
-// Express router middleware
-app.use(router)
+// Middlewares
+app.use(express.static('public')); // for static assets
+app.use(express.json()) // JSON parser(JSON => JS obj)
+app.use(router) // Router
+app.use(cookieParser()) // Cookie setting
 
 // database connection
 mongoose.connect(MONGO_URI, {  })
@@ -28,3 +26,28 @@ mongoose.connect(MONGO_URI, {  })
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+// create a cookie with cookie parser middleware
+app.get('/set-cookies', (req, res)=>{
+  res.cookie('newUser', false)
+  res.cookie('isChrome', true, {
+    // maxAge is about how long a cookie is stored. Default value of this
+    // is session, which means as soon as user turns off browser
+    // the cookie disaapear. Millisecond unit.
+    maxAge: 1000 * 60 * 60 * 24,
+
+    // secure option makes sure that cookie is only sent through https
+    secure: false,
+
+    // httpOnly option prevent front end Javascript code interfere with cookie.
+    httpOnly: true
+  })
+  res.send("this is cookie page")
+})
+
+// read a cookie
+app.get('/read-cookies', (req,res)=>{
+  const cookies = req.cookies
+  console.log(req.cookies.isChrome)
+  res.json(cookies)
+})
