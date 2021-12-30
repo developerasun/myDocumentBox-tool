28a990d60c895fec27a5ea322bf0d9faa61b4c57
designerasun <asuntheawesome@gmail.com>
2021-12-30 14:25:20 +0900

    ts component exercise

--- myJavascript/myReact/myComponents/ts-components/src/App.tsx
+++ myJavascript/myReact/myComponents/ts-components/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greetings } from './components/greetings';
+import { LoginForm } from './components/login'; 
+import { Person } from './components/person';
+import { PersonList } from './components/personList'
+import { Status } from './components/status'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <LoginForm />
       </header>
+      <main>
+        <Greetings 
+          name={"jake"} messageCount={50} />
+        <Person 
+          name={"jake"} age={27} nation='South Korea' />
+        <PersonList />
+
+        <Status status='loading'/>
+      </main>
     </div>
   );
 }
