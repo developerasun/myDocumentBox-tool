658c0ee706ee4e3f25c6117fb9dd73d7c3720986
designerasun <asuntheawesome@gmail.com>
2022-01-01 01:31:19 +0900

    componenets added

--- myJavascript/myReact/myComponents/ts-components/src/App.tsx
+++ myJavascript/myReact/myComponents/ts-components/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
 import './App.css';
 import { Greetings } from './components/greetings';
 import { LoginForm } from './components/login'; 
 import { Person } from './components/person';
 import { PersonList } from './components/personList'
 import { Status } from './components/status'
+import { Heading } from './components/heading'
+import { Oscar } from './components/oscar'
+import { Button } from './components/button'
+import { Input } from './components/input'
 
 function App() {
   return (
     <div className="App">
       <header>
         <LoginForm />
+        <Oscar>
+          <Heading>Hello</Heading> 
+        </Oscar>
       </header>
       <main>
         <Greetings 
@@ -20,6 +26,12 @@ function App() {
         <PersonList />
 
         <Status status='loading'/>
+
+        <Greetings 
+          name={"Brian"} messageCount={50} gender='male' />
+        <Button handleClick={(event)=>{console.log("button clicked", event)}}></Button>
+
+        <Input placeholder='Enter anything' handleChange={(event)=> console.log(event.target.value)} />
       </main>
     </div>
   );
