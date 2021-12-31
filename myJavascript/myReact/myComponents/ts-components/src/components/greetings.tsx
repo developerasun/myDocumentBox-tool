c6242954f426a433d2f9090b96d76064a842e839
designerasun <asuntheawesome@gmail.com>
2022-01-01 01:29:19 +0900

    optional prop in react component

--- myJavascript/myReact/myComponents/ts-components/src/components/greetings.tsx
+++ myJavascript/myReact/myComponents/ts-components/src/components/greetings.tsx
@@ -5,7 +5,8 @@ import { greetingStyle } from "../styles/styles"
 // use 'inferface' for building a library
 type GreetingProps = {
     name : string, 
-    messageCount : number
+    messageCount : number, 
+    gender? : string // gender prop is optional 
 }
 
 export const Greetings = (props:GreetingProps) => {
@@ -13,6 +14,7 @@ export const Greetings = (props:GreetingProps) => {
         <div style={greetingStyle.bgColor}> 
             <h2>Hello {props.name}, wecome back</h2>
             <p>You have {props.messageCount} unread messages</p>
+            <p>{props.gender}</p>
         </div> 
     )
 }
