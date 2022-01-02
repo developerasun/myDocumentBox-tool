905173396301130d046634281010562b8683f47d
designerasun <asuntheawesome@gmail.com>
2022-01-02 22:19:11 +0900

    production webpack bundle js added

--- myJavascript/myTypescript/public/webpack/dist.js
+++ myJavascript/myTypescript/public/webpack/dist.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log("hello webpack")})();
+(()=>{"use strict";console.log("hello webpack"),console.log("goodbye working?"),console.log("match index.html script src with webpack config publicPath"),console.log("imported")})();
