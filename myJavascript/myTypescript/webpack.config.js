79ae77cc7af1b79b237c220018651250d74938e3
designerasun <asuntheawesome@gmail.com>
2022-01-02 22:17:51 +0900

    devtool for debugging, webpack modes explained

--- myJavascript/myTypescript/webpack.config.js
+++ myJavascript/myTypescript/webpack.config.js
@@ -1,11 +1,14 @@
 // webpack files will be read by Node js
 // set input/output directory in webpack config.
 // 1) entry dir 2) output dir 
-
 const path = require('path')
 
 module.exports = {
+    devtool: 'eval-source-map', // use 'eval-source-map' for local dev debugging, use 'source-map' for production
     // entry is a relative path
+     // mode : 1) development (for local) 
+     // 2) production (publicsh your project) 
+     // 3) none Opts out of any default optimization options
     mode: 'development',
     entry : './src/webpack/index.ts',
     module : {
