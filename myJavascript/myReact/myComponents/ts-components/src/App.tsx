b129ae9fd188aa31691d91c2074db729603330f2
designerasun <asuntheawesome@gmail.com>
2022-01-04 14:14:16 +0900

    React.CSSproperties, useContext, class component, and component props added

--- myJavascript/myReact/myComponents/ts-components/src/App.tsx
+++ myJavascript/myReact/myComponents/ts-components/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Greetings } from './components/greetings';
 import { LoginForm } from './components/login'; 
+import { LoginTest } from './components/state/login'
 import { Person } from './components/person';
 import { PersonList } from './components/personList'
 import { Status } from './components/status'
@@ -8,6 +9,14 @@ import { Heading } from './components/heading'
 import { Oscar } from './components/oscar'
 import { Button } from './components/button'
 import { Input } from './components/input'
+import { Container } from './components/cssProps'
+import { ThemeContextProvider } from './components/context/themeContext'
+import { Box } from './components/context/box'
+import { UserContextProvider } from './components/context/userContextProvider'
+import { UserContextUser } from './components/context/userContext'
+import { ClassCounter } from './components/class/counter'
+import { Private } from './components/componentProp/private'
+import { Profile } from './components/componentProp/profile';
 
 function App() {
   return (
@@ -33,6 +42,34 @@ function App() {
 
         <Input placeholder='Enter anything' handleChange={(event)=> console.log(event.target.value)} />
       </main>
+      
+      <section>
+        <Container styles={{"backgroundColor" : "lightblue"}}/>
+      </section>
+
+      <section>
+        <LoginTest />
+      </section>
+
+      <section>
+        <ThemeContextProvider>
+          <Box />
+        </ThemeContextProvider>
+      </section>
+
+      <section>
+        <UserContextProvider >
+          <UserContextUser />
+        </UserContextProvider>
+      </section>
+
+      <section>
+        < ClassCounter message='hello'/>
+      </section>
+
+      <section>
+        <Private isLoggedIn={true} component={Profile} />
+      </section>
     </div>
   );
 }
