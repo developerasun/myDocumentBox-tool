927f639ad53967df773356a8dd2b976056eec81a
designerasun <asuntheawesome@gmail.com>
2022-01-06 12:58:12 +0900

    future value with useState

--- myJavascript/myReact/myComponents/ts-components/src/components/state/login.tsx
+++ myJavascript/myReact/myComponents/ts-components/src/components/state/login.tsx
@@ -6,17 +6,17 @@ import { UserProps } from "../propContainer";
 export function LoginTest () {
     // isLogin type is set based on initial value, which is boolean here
     // you don't have to explicitly say useState type thanks to type inference
-    const [user, setUser] = useState<UserProps | null>(null) 
+    const [user, setUser] = useState({} as UserProps)
     const handleLogin = () => setUser({
         name: "Jake", 
         email: "thisisjake@gmail.com",
         isLoggedIn: true
     })
-    const handleLogout = () => setUser(null)
+    const handleLogout = () => setUser({ name: "", email: "", isLoggedIn: false})
   return (
     <div>
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleLogout}>Logout</button>
+        <button onClick={handleLogin}>Login with setUser</button>
+        <button onClick={handleLogout}>Logout with setUser</button>
         {/* when state value by useState can be null, always have to add optioinal operator. 
         Use type assertion like below when you know the state won't be null. 
         const [user, setUser] = useState({} as UserProps)
