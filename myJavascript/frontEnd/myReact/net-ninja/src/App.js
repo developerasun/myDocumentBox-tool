b94b78d1df1403d85cc2ffb902ce1a6ad775177b
designerasun <asuntheawesome@gmail.com>
2022-01-07 13:53:32 +0900

    Query component commented out, Context API added

--- myJavascript/frontEnd/myReact/net-ninja/src/App.js
+++ myJavascript/frontEnd/myReact/net-ninja/src/App.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import Navbar from "./components/query/navbar";
-import nameContainer from "./components/query/stateNames/nameContainer";
-import RenderPlanet from "./components/query/planets";
-import RenderPeople from "./components/query/people";
+import Navbar from './components/contextHooks/navbar'
+import BookList from './components/contextHooks/bookList'
+import ThemeContextProvider from './components/contextHooks/context/themeContext'
 
 function App() {
-  const [page, setPage] = useState(nameContainer.setPageConst.planet)
-
+  
   return (
       <div className="App">
-        <Navbar setPage={setPage}/>
+
+        {/***************** React Context API and Hooks ****************/}
+
+        <ThemeContextProvider>
+          <Navbar />
+          <BookList />
+        </ThemeContextProvider>
+
+
+      
+
+
+
+        {/***************** React Query ****************/}
+        {/* // import Navbar from "./components/query/navbar";
+        // import nameContainer from "./components/query/stateNames/nameContainer";
+        // import RenderPlanet from "./components/query/planets";
+        // import RenderPeople from "./components/query/people"; */}
+        {/* const [page, setPage] = useState(nameContainer.setPageConst.planet) */}
+        {/* <Navbar setPage={setPage}/>
         <main>
           <h1 style={{"textAlign":"center"}}>React Query Exercise</h1>
           { page===nameContainer.setPageConst.planet 
                 ? <RenderPlanet/> : <RenderPeople /> }
-        </main>
+        </main> */}
       </div>
   );
 }
