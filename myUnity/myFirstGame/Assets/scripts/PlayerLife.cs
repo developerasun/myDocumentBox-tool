6a07be647cc2a7c6702838b91b046999746e4f0e
designerasun <asuntheawesome@gmail.com>
2022-01-10 00:38:24 +0900

    player death logic : at what point, what to disable(visibility, attached script, resetting position, and reloading scene

--- myUnity/myFirstGame/Assets/scripts/PlayerLife.cs
+++ myUnity/myFirstGame/Assets/scripts/PlayerLife.cs
@@ -5,27 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
-    string ENEMY_TAG = "enemyBody";
+    public string ENEMY_TAG = "enemyBody";
+    public string TRAPGROUND_TAG = "trapGround";
+    public float DROPLIMIT = 100f;
     private void OnCollisionEnter(Collision collision) 
     {
-        if (collision.gameObject.CompareTag(ENEMY_TAG))
+        if (collision.gameObject.CompareTag(ENEMY_TAG) | collision.gameObject.CompareTag(TRAPGROUND_TAG))
         {
-            UnityEngine.Debug.Log("player dead");
+            // try to find things to adjust when player considered dead
+            // bool Rigidbody.isKinematic : Controls whether physics affects the rigidbody.
+            GameObject.Find("SportsCar").transform.position =  new Vector3(1.37f, 1.11f, -6.75f);
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
             PlayerDead();
         }
     }
 
     void PlayerDead()
     {
-        // try to find things to adjust when player considered dead
-        // bool Rigidbody.isKinematic : Controls whether physics affects the rigidbody.
-        GameObject.Find("SportsCar").transform.localPosition =  new Vector3(0,0,-5);
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<PlayerMovement>().enabled = false;
+        UnityEngine.Debug.Log("player dead");
+        // Invoke : Invokes the method methodName in time seconds.
+        Invoke(nameof(ReloadLevel), 1f);
+        // ReloadLevel();
     }
 
     void ReloadLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // change scene name dynamically rather than hard coded
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -DROPLIMIT*3)
+        {
+            PlayerDead();
+        }
     }
 }
