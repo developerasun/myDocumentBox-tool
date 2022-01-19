c8dc78d5eda913cf696c216da064370f9ae9b252
designerasun <asuntheawesome@gmail.com>
2022-01-20 00:44:12 +0900

    file and dir reorganized/moved

--- myCSS/app.js
+++
@@ -1,8 +0,0 @@
-const darkModeButton = document.querySelector("#button-transition"); 
-
-function changeBgColor(e) { 
-    e.preventDefault();
-    document.body.style.backgroundColor = "white";  
-}
-
-darkModeButton.addEventListener("click", changeBgColor); 
