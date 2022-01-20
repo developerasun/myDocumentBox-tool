6f393fdaddbddca60dea67d10e9c7a06d75ef978
designerasun <asuntheawesome@gmail.com>
2022-01-20 19:34:07 +0900

    mock api with Promise added

---
+++ myJavascript/frontEnd/myReact/shopping-cart/src/fake.api.ts
@@ -0,0 +1,12 @@
+import { Product } from "./products/productSlice";
+
+const validateProduct = (product : Product):Promise<Product> => {
+    return new Promise((resolve, reject) => 
+    setTimeout(()=> {
+        if (product.title.length === 0) { reject('no title') }
+        if (product.price <= 0) { reject('incorrect price') }
+        resolve(product)
+    }, 500))
+}
+
+export default validateProduct
