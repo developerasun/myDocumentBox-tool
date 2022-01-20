9afabd8791068b48e85f8f0e738782052fd170e9
designerasun <asuntheawesome@gmail.com>
2022-01-20 19:29:52 +0900

    useState with multiple HTMLInputElement

--- myJavascript/frontEnd/myReact/shopping-cart/src/products/productForm.tsx
+++ myJavascript/frontEnd/myReact/shopping-cart/src/products/productForm.tsx
@@ -1,16 +1,46 @@
 import * as React from 'react';
+import { useAppDispatch } from '../store';
+import { addProduct, Product } from './productSlice';
 
-export interface IProductFormProps {
-}
+// state in form recommended to be local, not in Redux store.
+export function ProductForm () {
 
-export function ProductForm (props: IProductFormProps) {
+  const dispatch = useAppDispatch()
+  const [product, setProduct] = React.useState<Product>({
+    id : '',
+    title : '',
+    price : 0 
+  })
+
+  // when single HTMLInputElement state changes => event : React.ChangeEvent<HTMLInputElement>
+  // when multiple HTMLInputElement => { target : { name, value }} : React.ChangeEvent<HTMLInputElement>
+  const handleChange = ( { target : { name, value }} : React.ChangeEvent<HTMLInputElement> ) => {
+
+    // setProduct takes an argument, which is a previous state(Product)
+    setProduct( prev => { 
+      
+      // Object[input name] = input value
+      (prev as any)[name] = value
+      const newValue = { ...prev }
+      return newValue
+    })
+  }
+
+  const handleSubmit = (event : React.FormEvent) => {
+    event.preventDefault() // prevent that input values are displayed in URL
+    console.log( product.id, product.title, product.price )
+    dispatch(addProduct(product))
+  }
+
   return (
     <div>
       <h2>Add Game To Store</h2>
-      <form>
-        <input type="text" name="" id="" placeholder='title'/>
-        <input type="text" name="" id="" placeholder='price'/>
-        <button>Add price</button>
+      <form onSubmit={handleSubmit}>
+        {/* input name and value are used above useState(setState) */}
+        <input type="text" name="title" id="title" placeholder='title' value={product.title} onChange={handleChange} />
+        <input type="text" name="price" id="price" placeholder='price' value={product.price} onChange={handleChange} />
+        <input type="text" name="id" id="id" placeholder='id' value={product.id} onChange={handleChange} />
+        <button type='submit'>Add price</button>
       </form>
     </div>
   );
