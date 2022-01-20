a256eaf04f93818ff35e9c59bd56870c361d9db7
designerasun <asuntheawesome@gmail.com>
2022-01-20 19:30:40 +0900

    maps out game item list with Redux

--- myJavascript/frontEnd/myReact/shopping-cart/src/products/productList.tsx
+++ myJavascript/frontEnd/myReact/shopping-cart/src/products/productList.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { v4 } from 'uuid';
-import { getProductSelector } from './productSlice';
+import { addToCart } from '../cart/cart.slice';
+import { useAppDispatch } from '../hooks/store.hooks';
+import { getProductSelector, Product, removeProduct } from './productSlice';
 
-export interface  ProductListProps {
-}
-
-export function ProductList (props:  ProductListProps) {
+export function ProductList () {
 
     // A hook to access the redux store's state. 
     // This hook takes a selector function as an argument. 
     // The selector is called with the store state.
     const product = useSelector(getProductSelector)
+    const dispatch = useAppDispatch()
+    const removeFromStore = (id : string) => {
+      // Dispatches a synthetic event event to target
+      dispatch(removeProduct(id))
+    }
+
+    const addToCartHandler = (product:Product) => {
+      dispatch(addToCart(product))
+    }
+
 
   return (
     <div>
@@ -19,7 +28,9 @@ export function ProductList (props:  ProductListProps) {
         {product.map((game) => {
             return (
             <div key={v4()}>
-            <span>{game.title} is {game.price} dollars.</span>
+              <span>{game.title} is {game.price} dollars.</span>
+              <button onClick={()=>addToCartHandler(game)}>Add to cart</button>
+              <button onClick={()=>removeFromStore(game.id)}>Remove from store</button>
             </div>)  })}
     </div>
   );
