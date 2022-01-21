ddc0861ecad2185bb97e0fbe525bbd62ffebbab8
designerasun <asuntheawesome@gmail.com>
2022-01-21 13:05:33 +0900

    extraReducers fixed, getErrorMessage state adde

--- myJavascript/frontEnd/myReact/shopping-cart/src/products/productSlice.tsx
+++ myJavascript/frontEnd/myReact/shopping-cart/src/products/productSlice.tsx
@@ -1,8 +1,9 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction, createEntityAdapter } from '@reduxjs/toolkit'
 import { v4 } from 'uuid'
 import validateProduct from '../fake.api'
 import { RootState } from '../store'
 
+// Product setting
 export interface Product {
     title : string
     price : number
@@ -15,12 +16,14 @@ export const initialProduct : Product[] = [
     { title : "game 3", price : 222, id : v4() },
 ]
 
+// fake api validation setting : enum classifies validation state by number
 export enum ValidationState {
-    Fulfilled,
-    Pending,
-    Rejected
+    Fulfilled, // 0
+    Pending, // 1
+    Rejected // 2
 }
 
+// reducer(createSlice) setting
 interface ProductSliceState {
     product : Product[]
     validationState? : ValidationState
@@ -33,63 +36,50 @@ const initialState : ProductSliceState = {
     errorMessage : undefined
 }
 
-// For Redux specifically, "thunks" are a pattern of writing functions with logic
-// inside that can interact with a Redux store's dispatch and getState methods.
-// Thunks are the standard approach for writing async logic in Redux apps, 
-// and are commonly used for data fetching. However, they can be used 
-// for a variety of tasks, and can contain both synchronous and asynchronous logic.
-export const addProductAsync = createAsyncThunk('product/addNewProduct', async (initialProduct : Product)=>{
+// const productAdapter = createEntityAdapter<Product>()
+// const initalState = 
+
+// async request setting with the fake api
+export const addProductAsync = createAsyncThunk('products/addNewProduct', async (initialProduct : Product)=>{
     const validatedProduct = await validateProduct(initialProduct)
     return validatedProduct
 })
 
-// createSlice creates your actions as well as your reducer
+// createSlice : slice name, initial state, reducer's' object, extraReducer(for async request)
 const productSlice = createSlice({
     name: 'products',
     initialState  : initialState, 
     reducers : {
-        // PayloadAction : An action with a string type and an associated payload. 
-        // This is the type of action returned by createAction() action creators.
         addProduct : (state, action: PayloadAction<Product>) => {
             // return [action.payload, ...state]
             state.product.push(action.payload) // immutable.js are being developed for simpler state management
         }, 
         removeProduct : (state, action: PayloadAction<string>) =>({
             ...state, 
-            // The filter() method creates a new array with all elements 
-            // that pass the test implemented by the provided function
             product : state.product.filter(product => product.id !== action.payload)
         }), 
-    extraReducers: builder => {
-        
-    }
+    }, 
+    extraReducers : builder => {
+        builder.addCase(addProductAsync.fulfilled, (state, action) => ({
+            ...state, 
+            validationState : ValidationState.Fulfilled, 
+            errorMessage : undefined, 
+            product : [ ...state.product, action.payload ]
+        }))
+        builder.addCase(addProductAsync.rejected, (state, action)=>({
+            ...state, 
+            validationState : ValidationState.Rejected, 
+            errorMessage : action.error.message
+        }))
+        builder.addCase(addProductAsync.pending, (state, action)=>({
+            ...state, 
+            validationState : ValidationState.Pending, 
+            errorMessage : undefined
+        }))
     }
 })
 
-
-// Action creators for the types of actions that are handled by the slice reducer.
 export const { addProduct, removeProduct } = productSlice.actions
-
-export const getProductSelector = ( state: RootState ) => state.product
-
+export const getProductSelector = ( state: RootState ) => state.product.product
+export const getErrorMessage = ( state : RootState ) => state.product.errorMessage
 export default productSlice.reducer
-
-
-// (builder) => {
-//     builder.addCase(addProductAsync.fulfilled, (state, action) => ({
-//         ...state, 
-//         validationState : ValidationState.Fulfilled, 
-//         errorMessage : undefined,
-//         product : [...state.product, action.payload]
-//     }))
-//     builder.addCase(addProductAsync.rejected, (state, action) => ({
-//         ...state, 
-//         validationState : ValidationState.Rejected, 
-//         errorMessage : action.error.message,
-//     }))
-//     builder.addCase(addProductAsync.pending, (state, action) => ({
-//         ...state, 
-//         validationState : ValidationState.Pending, 
-//         errorMessage : undefined,
-//     }))
-// }
