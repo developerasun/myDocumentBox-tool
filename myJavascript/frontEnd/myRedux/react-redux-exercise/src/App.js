841ec37074d3d58e426d8d4f809158011f5bf067
designerasun <asuntheawesome@gmail.com>
2022-01-24 15:02:26 +0900

    comp with hook added

--- myJavascript/frontEnd/myRedux/react-redux-exercise/src/App.js
+++ myJavascript/frontEnd/myRedux/react-redux-exercise/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import CakeContainer from './components/cakeContainer';
+import HooksCakeContainer from './components/hooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
