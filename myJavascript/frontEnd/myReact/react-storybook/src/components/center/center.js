6ba98bfde0d06444baddc5eb40dc1886429e87ad
designerasun <asuntheawesome@gmail.com>
2022-01-31 15:44:03 +0900

    feat : adding wrapper center component

---
+++ myJavascript/frontEnd/myReact/react-storybook/src/components/center/center.js
@@ -0,0 +1,11 @@
+import './center.css'
+
+function Center(props) {
+    return ( 
+        <div className="center">
+            {props.children}
+        </div>
+     );
+}
+
+export default Center;
