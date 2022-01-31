c3145a09d900eb4d441ef28d7a7ec95c96608081
designerasun <asuntheawesome@gmail.com>
2022-01-31 15:43:06 +0900

    feat : adding chakra ui button as Storybook Template

---
+++ myJavascript/frontEnd/myReact/react-storybook/src/components/ChakraButton/chakraButton.stories.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+
+export default {
+    title : 'Chakra/Button', 
+    component : Button,
+    argTypes : {
+        onClick : { action : 'clicked' }
+    }
+}
+
+const Template = args => <Button {...args} />
+
+export const Success = Template.bind({})
+Success.args = {
+    backgroundColor : 'green',
+    color : 'white',
+    padding : '20',
+    children : 'Success'
+}
+
+export const Danger = () => <Button backgroundColor={'red'} color={'white'} padding={'20'}>Danger</Button>
