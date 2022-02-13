11062b6eb90fcc70f59dd273e1b36bfb1f8428f1
Jake Sung <asuntheawesome@gmail.com>
2022-02-13 17:53:19 +0900

    Delete myDeploy directory

--- myDeploy/netlify/serverless-netlify/functions/hello.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async (event, context) => {
-    return { 
-        statusCode : 200, 
-        body : JSON.stringify({ message : "hello serverless, I'm coming from backend" })
-    }
-}
